4c0ca2a28452a7af9688d67f406b11e2b300cb09
Armaan Gomes <acgo8888@gmail.com>
2024-01-07 23:33:44 -0800

    pain

--- beamformingarray/MVDR.py
+++ beamformingarray/MVDR.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-
+from Signal import Sine
+from SignalGen import SignalGen
 class MVDR:
     def __init__(self,vector_gen, sample_rate=48000):
         self.vector_gen = vector_gen
         self.sample_rate=sample_rate
     def get_peaks(self, frame):
+        frame=(self.convert_to_complex(frame)).T
         thetas= np.arange(-180,180,1)
         res=[]
         for theta in thetas:
@@ -15,15 +17,25 @@ class MVDR:
             cov_inv=np.linalg.pinv(cov)
             cost=1/(look.H@cov_inv@look)
             cost=np.abs(cost[0,0])
-            cost=10*np.log10(cost)
+            cost = 10*np.log10(cost)
             res.append(cost)
         res/=np.max(res)
+        peaks=thetas[(signal.find_peaks(res)[0])]
+        print(peaks)
         fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
         ax.plot(np.radians(thetas), res) # MAKE SURE TO USE RADIAN FOR POLAR
         ax.set_theta_zero_location('N') # make 0 degrees point up
         ax.set_theta_direction(-1) # increase clockwise
         ax.set_rlabel_position(30)  # Move grid labels away from other labels
         plt.show()
+            # print(thetas[peaks[i]])
+    def convert_to_complex(self,frame):
+        arr=np.matrix(frame.astype(complex))
+        # print(arr)
+        # for i in range(len(arr)):
+        #     print(arr[i])
+        return arr
+
 
 
 
@@ -35,25 +47,14 @@ class Look_Vector_Generator:
     def get_look_vector(self,theta):
         return (np.asmatrix(np.exp(-2j*np.pi*self.r_pos*np.sin(np.radians(theta))))).T
 d=0.15
-r_pos=np.arange(4)*d
+Nr=8
+r_pos=np.arange(Nr)*d
 look_gen=Look_Vector_Generator(r_pos)
 mvdr=MVDR(look_gen)
-sample_rate = 48000
-N = 10000 # number of samples to simulate
+sig=Sine(2000)
+gen=SignalGen(Nr,d)
+gen.update_delays(90)
 
-# Create a tone to act as the transmitted signal
-t = np.arange(N)/sample_rate
-Nr = 8 # 8 elements
-theta1 = -40 / 180 * np.pi # convert to radians
-theta2 = 30 / 180 * np.pi
-theta3 = -40 / 180 * np.pi
-a1 = np.asmatrix(np.exp(-2j * np.pi * d * np.arange(Nr) * np.sin(theta1)))
-a2 = np.asmatrix(np.exp(-2j * np.pi * d * np.arange(Nr) * np.sin(theta2)))
-a3 = np.asmatrix(np.exp(-2j * np.pi * d * np.arange(Nr) * np.sin(theta3)))
-# we'll use 3 different frequencies
-r = a1.T @ np.asmatrix(np.exp(2j*np.pi*500*t)) + \
-    a2.T @ np.asmatrix(np.exp(2j*np.pi*1000*t)) + \
-    0 * a3.T @ np.asmatrix(np.exp(2j*np.pi*2000*t))
-n = np.random.randn(Nr, N) + 1j*np.random.randn(Nr, N)
-r = r + 0.04*n
+r=(gen.delay_and_gain(sig.generate_wave(0.1)))
+
 mvdr.get_peaks(r)
