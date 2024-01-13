144ca93b27f64b70231cf24189e557883a6fe279
Armaan Gomes <acgo8888@gmail.com>
2024-01-12 21:37:22 -0800

    compiles but fails

--- beamformingarray/MVDR.py
+++ beamformingarray/MVDR.py
@@ -27,35 +27,37 @@ output=np.zeros((N,1))
 N_f=int(stft_len/2+1)#Num frequencies
 global_covar=np.zeros((num_channel,num_channel,N_f))
 frame_count=1
-j=1
+j=0
 mu=0
 while j + frame_len < N:
-    print(j)
-    win_data = pcm[j : j + frame_len,:]*win_multi
-    spectrum=fft.fft(win_data,stft_len)
+    # print(pcm[j : j + frame_len,:].shape)
+    win_data = np.asmatrix(pcm[j : j + frame_len,:]*win_multi)
+    spectrum=np.asmatrix(fft.fft(win_data,stft_len,axis=0))
     if frame_count < exp_avg_param:
         mu=(frame_count-1)/frame_count
-    for k in range(1,N_f):
-        cov_mat=np.dot(spectrum[k,:].T,np.conj((spectrum[k,:])))
+    for k in range(0,N_f):
+        cov_mat=np.dot(spectrum[k,:].T, np.conj(spectrum[k,:]))
         corr_mat=cov_mat/np.trace(cov_mat); 
+        # print(cov_mat.shape)
         global_covar[:,:,k]=mu*global_covar[:,:,k]+(1-mu)*corr_mat
-    time=np.zeros(1,num_channel)
-    w=np.zeros(num_channel,N_f)
+    time=np.asmatrix(np.zeros((1,num_channel)))
+    w=np.asmatrix(np.zeros((num_channel,N_f)))
     for k in range (0,N_f-1):
         f=k*FS/stft_len;
         alpha=np.exp(-j*2*np.pi*f*time).T
         r_inv=np.linalg.pinv(global_covar[:,:,k+1]+(1e-8)*np.diag(np.ones((num_channel,1))))
         w[:,k+1]=r_inv*alpha/(np.conj(alpha.T)*r_inv*alpha)
-    rec_signal=np.conj(w.T)*spectrum[1:N_f,:]# check line
-    rec_signal=[rec_signal]
+    rec_signal=np.multiply(np.conj(w.T),spectrum[0:N_f,:])# check line
+    # rec_signal=[rec_signal]
+    print(j)
     #### CHECK THIS
     submatrix = rec_signal[1:-1, :]
     flipped_conjugate = np.flipud(np.conj(submatrix))
     rec_signal = np.vstack([rec_signal, flipped_conjugate])
     ####    
     res=np.real(np.fft.ifft(np.sum(rec_signal,axis=1)))
-    res=res[1:frame_len]
-    output[j:j+frame_len-1,:]=output[j:j+frame_len-1,:]+res
+    res=res[0:frame_len]
+    output[j:j+frame_len,:]=output[j:j+frame_len,:]+res
     
     frame_count = frame_count + 1; 
 
