b8395593596f0b4b24468a899f413c15689a3f9d
Armaan Gomes <acgo8888@gmail.com>
2024-02-13 15:52:08 -0800

    simple delay approximator

---
+++ beamformingarray/DelayApproximation.py
@@ -0,0 +1,24 @@
+# Make something where oyu pass in source coords and the coord of the mics and then i calculates the time diff from each mic from the first mic
+import numpy as np
+v=343.3
+class DelayAproximator:
+
+    def __init__(self,coords) :
+        self.coords=coords
+
+    def get_delays(self,pos):
+        distances = []
+        for mic_pos in self.coords:
+            dx = mic_pos[0] - pos[0]
+            dy = mic_pos[1] - pos[1]
+            distance = np.sqrt(dx**2 + dy**2)
+            distances.append(distance)
+        reference_distance = self.get_distances()[0]
+        tdoa_values = []
+        for distance in self.get_distances()[1:]:
+            time_diff = (distance - reference_distance) / v
+        tdoa_values.append(time_diff)
+        return tdoa_values
+    
+
+
